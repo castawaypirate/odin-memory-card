a2b4b1635d584d53322c5042980282f591609453
castawaypirate <apollonas@csd.auth.gr>
2026-07-08 11:59:08 +0300

    style: use remaining CSS custom properties for fonts and colors

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,8 +1,14 @@
+import "../styles/Score.css";
+
 export function Score({ score, bestScore }) {
   return (
     <div className="score-board">
-      <h5>Score: {score}</h5>
-      <h5>Best Score: {bestScore}</h5>
+      <h5>
+        <span>Score:</span> {score}
+      </h5>
+      <h5>
+        <span>Best Score:</span> {bestScore}
+      </h5>
     </div>
   );
 }
